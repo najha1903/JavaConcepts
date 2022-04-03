8c143b3afdeea728c1603e5858ee9cbc3ad1b055
Navneet Jha <navneetjha500@gmail.com>
2022-04-03 19:06:59 +0530

    Added new coding challenges

--- src/PrimitiveTypes/PrimitiveDataTypes.java
+++ src/PrimitiveTypes/PrimitiveDataTypes.java
@@ -30,6 +30,15 @@ package PrimitiveTypes;
 
 //Casting in Java :- Casting means to treat or convert from one dataType to another.
 
+// In Java, there are two types of casting:
+//
+//        Widening Casting (automatically) - converting a smaller type to a larger type size
+//        byte -> short -> char -> int -> long -> float -> double
+//
+//        Narrowing Casting (manually) - converting a larger type to a smaller size type
+//        double -> float -> long -> int -> char -> short -> byte
+
+
 //Char DataType can store only one character in a single char variable. Char values are defined under single quotes.
 
 //A Char occupies two bytes of memory, or 16 bits, and thus has width of 16.
